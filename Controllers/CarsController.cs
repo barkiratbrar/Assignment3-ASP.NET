89720661c521694ee6b1ee09bc41af69ef87156c
Barkirat Singh Brar <138818899+barkiratbrar@users.noreply.github.com>
2024-04-15 21:29:03 -0400

    Add files via upload

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AutoDealer_Assignment3.Controllers
+{
+    public class CarsController : Controller
+    {
+        // Action method for the Index view
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Action method for the About view
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        // Action method for the Services view
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+    }
+}
+
---
+++ Models/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AutoDealer_Assignment3.Models
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
+
